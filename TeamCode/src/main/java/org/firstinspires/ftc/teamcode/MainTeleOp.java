4d8031e0ed6a544d17977d88c2b99e784f2c853a
yashg4509 <bludgermoonstone66@gmail.com>
2019-12-14 15:32:48 -0800

    linear actuator movement and deletion of comments update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -14,11 +14,6 @@ public class MainTeleOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap); //giving robot hardware map config
 
-//        boolean toggleRot = true;
-//       boolean toggleIntake = false;
-//
-//        double clawOpenPos = 1;
-//        double clawClosePos = 0.5;
 
         waitForStart(); //when start is pressed opModeIsActive returns true; will return false when stopped
 
@@ -37,136 +32,17 @@ public class MainTeleOp extends LinearOpMode {
             boolean openClaw = gamepad1.right_bumper;
             boolean dropClaw = gamepad1.a;
             boolean raiseClaw = gamepad1.b;
+            
+            boolean forwardLinActuator = gamepad1.x;
+            boolean backLinActuator = gamepad1.y;
 
 
-//            double intake = (double)gamepad1.left_trigger;
-//            boolean release = gamepad1.left_bumper;
-//
-//            boolean rotIntake = gamepad1.left_bumper;
-//
-//
-//            boolean liftUp = gamepad1.right_bumper;
-//            boolean liftDown = (gamepad1.right_trigger > 0);
-
-            //double y2 = gamepad1.right_stick_y;
-/*
-            boolean claw1Open = gamepad1.left_bumper;
-            boolean claw1Close = (gamepad1.left_trigger > 0);
-            boolean claw2Open = gamepad1.right_bumper;
-            boolean claw2Close = (gamepad1.right_trigger > 0);
-//*/
-//            boolean clawUp = gamepad1.y;
-//            boolean clawDown = gamepad1.a;
-
-//            boolean intake = (gamepad1.left_trigger > 0);
 
             double bl = y1 - x1 + x2;
             double br = y1 + x1 - x2;
             double fl = y1 - x1 - x2;
             double fr = y1 + x1 + x2;
 
-
-
-            /*
-            bl = Math.pow(bl, 3);
-            br = Math.pow(br, 3);
-            fl = Math.pow(fl, 3);
-            fr = Math.pow(fr, 3);
-            */
-
-
-/*
-            double max = Math.max(Math.max(Math.abs(bl), Math.abs(br)), Math.max(Math.abs(fl), Math.abs(fr)));
-            if(max > 1.0){
-                bl /= max;
-                br /= max;
-                fl /= max;
-                fr /= max;
-            }
-            if(Math.abs(bl) < 0.5) bl = 0;
-            if(Math.abs(br) < 0.5) br = 0;
-            if(Math.abs(fl) < 0.5) fl = 0;
-            if(Math.abs(fr) < 0.5) fr = 0;
-
-            bl = bl>=0? Math.pow(bl,2):-Math.pow(bl,2);
-            br = br>=0? Math.pow(br,2):-Math.pow(br,2);
-            fl = fl>=0? Math.pow(fl,2):-Math.pow(fl,2);
-            fr = fr>=0? Math.pow(fr,2):-Math.pow(fr,2);
-*/
-
-
-//            if (clawUp) {
-//                robot.armBot.setPower(1);
-//            }
-//            if (clawDown) {
-//                robot.armBot.setPower(-1);
-//            }
-
-//            if (rotYAdd) {
-//                robot.armTop.setPower(0.6);
-//            }
-//            if (rotYSub) {
-//                robot.armTop.setPower(-0.6);
-//            }
-
-//            if (liftUp){
-//                robot.liftMotor.setPower(-1);
-//            }
-//            if (liftDown){
-//                robot.liftMotor.setPower(1);
-//            }
-//            if (release) {
-//                robot.intakeServo.setPosition(1);
-//
-//            }
-
-
-//            if (intake){
-//                robot.intakeServo.setPower(-1);
-//            }
-
-
-
-            /*
-            if (rotZAdd) {
-                servoZRot = servoZRot + 0.01;
-                servoZRot = servoZRot>1? 1: servoZRot;
-                robot.servoClawBot.setPosition(servoZRot);
-                sleep(10);
-            }
-            if (rotZSub) {
-                servoZRot = servoZRot - 0.01;
-                servoZRot = servoZRot<0? 0: servoZRot;
-                robot.servoClawBot.setPosition(servoZRot);
-                sleep(10);
-            }
-            if (intake) {
-                robot.intakeServo.setPosition(0.3);
-                //sleep(500);
-            }
-            if (rotIntake){
-                if (toggleRot){
-                    toggleRot = !toggleRot;
-                    sleep(500);
-                    robot.rotationServo.setPosition(0.15);
-                    sleep(100);
-                }
-                else if (!toggleRot){
-                    toggleRot = !toggleRot;
-                    sleep(500);
-                    robot.rotationServo.setPosition(1);
-                    sleep(100);
-                }
-            }
-*/
-
-//
-//            robot.armBot.setPower(0);
-//            robot.armTop.setPower(0);
-//            robot.liftMotor.setPower(0);
-////            robot.intakeServo.setPower(0);
-//
-//            robot.intakeServo.setPosition(0.5-(intake/2));
 
 
             robotDrive(bl, br, fl, fr);
@@ -187,13 +63,6 @@ public class MainTeleOp extends LinearOpMode {
                 raiseClaw();
             }
 
-
-
-/*
-            telemetry.addData("servotoppos", robot.servoClawTop.getPosition());
-            telemetry.addData("servobotpos", robot.servoClawBot.getPosition());
-            telemetry.update();
-*/
 
 
         }
@@ -257,6 +126,10 @@ public class MainTeleOp extends LinearOpMode {
     public void closeClaw () {
         robot.leftClawServo.setPosition(-85);
         robot.rightClawServo.setPosition(85);
+    }
+
+    public void forwardLinearActuator(double power) {
+
     }
 
 }
